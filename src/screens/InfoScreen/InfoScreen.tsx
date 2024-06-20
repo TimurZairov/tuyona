c167082a89086d07e6c566756fbfd97e4a34afca
Тимур Заиров <zairovne@gmail.com>
2024-06-20 23:14:22 +0200

    info screen

--- src/screens/InfoScreen/InfoScreen.tsx
+++ src/screens/InfoScreen/InfoScreen.tsx
@@ -1,11 +1,64 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
-import {COLORS, SIZES, height} from '../../theme/theme';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {COLORS, SIZES, height, width} from '../../theme/theme';
 import ServiceCard from '../../components/ServiceCard/ServiceCard';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {useNavigation} from '@react-navigation/native';
 
 const InfoScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.info}>
+      {/* Navigation */}
+      <View style={styles.header}>
+        {/* GO BACK */}
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.btn}>
+          <Ionicons
+            name="chevron-back"
+            size={SIZES.large}
+            color={COLORS.mainColor}
+          />
+        </TouchableOpacity>
+        {/* CONTact */}
+        <View style={styles.contact}>
+          {/*  */}
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.btn}>
+            <Ionicons
+              name="heart-outline"
+              size={SIZES.large}
+              color={COLORS.mainColor}
+            />
+          </TouchableOpacity>
+          {/*  */}
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.btn}>
+            <AntDesign
+              name="phone"
+              size={SIZES.large}
+              color={COLORS.mainColor}
+            />
+          </TouchableOpacity>
+          {/*  */}
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.btn}>
+            <AntDesign
+              name="sharealt"
+              size={SIZES.large}
+              color={COLORS.mainColor}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* MAIN */}
       <Image
         source={{
           uri: 'https://repost.uz/storage/uploads/file_5b10456e434296.773592021527793006.jpg',
@@ -30,6 +83,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: COLORS.grayColor,
   },
+  header: {
+    position: 'absolute',
+    top: 50,
+    paddingHorizontal: 8,
+    zIndex: 30,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  btn: {
+    backgroundColor: 'rgba(1, 1, 1, 0.25)',
+    padding: 10,
+    borderRadius: 20,
+  },
+  contact: {
+    width: width / 3,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
   image: {
     height: height / 2.5,
     borderBottomStartRadius: 20,
